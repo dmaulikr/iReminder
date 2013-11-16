79877986d98ceb8b5f3764a1d43cbb380bfac470
Ruben Fernandez <ruben@rffsystems.es>
2013-11-16 15:23:52 +0100

    Initial Commit

---
+++ iReminder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iReminder
+//
+//  Created by Ruben Fernandez Fuste on 16/11/13.
+//  Copyright (c) 2013 Rff Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iReminder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iReminder
+//
+//  Created by Ruben Fernandez Fuste on 16/11/13.
+//  Copyright (c) 2013 Rff Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
